f2873b3bb86ff8b32ac81d872fb1aefa0d066228
PROROKDG <domenforsites@gmail.com>
2021-12-23 19:43:00 +0300

    first commit

--- app.js
+++ app.js
@@ -7,3 +7,15 @@ const App = {
 }
 
 Vue.createApp(App).mount("#app")
+/*
+let acl = new Accelerometer({frequency: 60});
+acl.addEventListener('reading', () => {
+    var xyz = [acl.x,acl.y,acl.z]
+    let cords = document.querySelector('#cords')
+    for (i=0;i<3;i++){
+    cords.textContent = i+1,". ",xyz[i];
+}
+});
+
+acl.start();
+*/
